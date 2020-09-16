63b3ac4d0b48eebe866c6637f5b88126ee2dcf58
hansenj <hansen.j@husky.neu.edu>
2020-09-16 12:10:27 -0400

    Creates xjson, executable, inline tests, no test directory tests yet

---
+++ C/Other/xjson.rkt
@@ -0,0 +1,66 @@
+#lang racket/base
+
+(require json
+         racket/string
+         racket/port
+         rackunit)
+
+(provide xjson)
+
+;+---------------------------------------------------------------------------------------------------+
+; Constants
+
+(define CNT 'count)
+(define SEQ 'seq)
+
+(define TEST-JSON '(1 "a" #hasheq((hello . "world")) #hasheq((outer . #hasheq((inner . "layer"))))))
+(define TEST-JSON-STR "1 \"a\" {\"hello\": \"world\"} {\"outer\": {\"inner\": \"layer\"}}")
+
+;+---------------------------------------------------------------------------------------------------+
+; Functions
+
+; -> [Listof jsexpr?]
+; Build list of json inputs until eof
+(define (read-input)
+  (define json-obj (read-json))
+  (if (eof-object? json-obj) '() (cons json-obj (read-input))))
+(check-equal? (with-input-from-string "" read-input) '())
+(check-equal? (with-input-from-string "1" read-input) '(1))
+(check-equal? (with-input-from-string TEST-JSON-STR read-input) TEST-JSON)
+
+; [Listof jsexpr?] -> [Hashof symbol? jsexpr?]
+; Create a hashmap with the count and original sequence
+(define (count-seq json-list)
+  (hash
+   CNT (length json-list)
+   SEQ json-list))
+(check-equal? (count-seq '()) (hash CNT 0 SEQ '()))
+(check-equal? (count-seq TEST-JSON) (hash CNT 4 SEQ TEST-JSON))
+
+; [Listof jsexpr?] -> [Listof jsexpr?]
+; Cons the number of json objects to the reversed list
+(define (count-and-reverse json-list)
+  (cons
+   (length json-list)
+   (reverse json-list)))
+(check-equal? (count-and-reverse '()) '(0))
+(check-equal? (count-and-reverse TEST-JSON) (cons 4 (reverse TEST-JSON)))
+
+; Read json from STDIN until EOF, then write two json objects
+; - A json object with the count of json objects and the orignal sequence
+; - A json list with the count as the first element and the json sequence in reversed order
+(define (xjson . a)
+  (define json-list (read-input))
+  (newline)
+  (write-json (count-seq json-list))
+  (newline)
+  (write-json (count-and-reverse json-list))
+  (newline))
+(check-equal?
+ (with-output-to-string (λ () (with-input-from-string "" xjson)))
+ "\n{\"count\":0,\"seq\":[]}\n[0]\n")
+(check-equal?
+ (map string->jsexpr
+      (string-split (with-output-to-string (λ () (with-input-from-string TEST-JSON-STR xjson))) "\n"))
+ (list (hasheq CNT 4 SEQ TEST-JSON)
+       (cons 4 (reverse TEST-JSON))))

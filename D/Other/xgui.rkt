97e11c94e6a112b94daf493e2605d11d555272ff
hansenj <hansen.j@husky.neu.edu>
2020-09-20 22:58:25 -0400

    Command line parsing

--- D/Other/xgui.rkt
+++ D/Other/xgui.rkt
@@ -1,13 +1,34 @@
 #lang racket
 
-(require rackunit)
+(require racket/cmdline
+         rackunit)
 
 (provide xgui)
 
 ;+---------------------------------------------------------------------------------------------------+
 ; Constants
 
+(define ERR-NUM-ARGS "Incorrect number of arguments.\nCorrect usage: ./xgui [positive integer]")
+(define ERR-BAD-SIZE
+  (string-append "Invalid size, must be an integer greater than 0.\n"
+                 "Correct usage: ./xgui [positive integer]"))
 ;+---------------------------------------------------------------------------------------------------+
 ; Functions
 
-(define (xgui args) ...)
+; Ensures the input is a single positive integer
+; Return value should be ignored
+; [Listof Str] -> Bool
+(define (check-legal-input args)
+  (cond [(not (= 1 (length args)))                       (error ERR-NUM-ARGS)]
+        [(false? (string->number (first args)))          (error ERR-BAD-SIZE)]
+        [(not (positive? (string->number (first args)))) (error ERR-BAD-SIZE)]
+        [else #t]))
+
+; Draws a hexagon, closes when the hexagon is clicked
+(define (run-hex size)
+  (big-bang #t ...))
+
+(define (xgui args)
+  (check-legal-input args)
+  (define size (string->number (first args)))
+  (println size))

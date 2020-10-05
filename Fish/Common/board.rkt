477db6ad7b1c03d0042dc4ed526a7f47d7e576e6
hansenj <hansen.j@husky.neu.edu>
2020-10-05 18:52:19 -0400

    Adds board documentation and some board functions

--- Fish/Common/board.rkt
+++ Fish/Common/board.rkt
@@ -1,23 +1,38 @@
 #lang racket/base
 
-(require "tile.rkt")
+(require "tile.rkt"
+         racket/contract
+         lang/posn)
+
+;; A Board is a (vectorof (vectorof tile?)) and uses a doubled coordinate system to represent a grid
+;; of tesselated hexagons. The coordinates for a Board following format:
+#|
+(0,0)     (2,0)     (4,0) ... (n,0)
+     (1,1)     (3,1)      
+(0,2)     (2,2)     (4,2)     (n,2)
+     (1,3)     (3,3)
+(0,4)     (2,4)     (4,4)     (n,4)
+...                            ...
+(0,m)     (2,m)     (4,m) ... (n,m)
+|#
 
 ;; make-random-board: natural? natural? (listof posn?) ??? -> board?
 #;
 (define (make-random-board width height holes ...)
   ...)
 
-;; make-even-board: natural? natural? natural? ??? -> board?
-#;
-(define (make-even-board: width height fish ...)
-  ...)
+;; make-even-board: natural? natural? tile? -> board?
+(define (make-even-board width height tile)
+  (build-vector width (λ (x) (make-vector height tile))))
 
 ;; reachable-posns : board? posn? -> (listof posn?)
 #;
 (define (reachable-posns board current)
   ...)
 
-;; remove-tile : board? tile? -> board?
-#;
-(define (remove-tile board tile)
-  ...)
+;; remove-tile : board? posn? -> board?
+(define (remove-tile board posn)
+  (vector-set! (vector-ref board (posn-x posn))
+               (posn-y posn)
+               0)
+  board)

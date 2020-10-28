466afa491486b1d1725ab7396d85f383bfc20392
hansenj <hansen.j@husky.neu.edu>
2020-10-28 16:55:40 -0400

    Finishes strategy.rkt unit tests

--- Fish/Player/strategy.rkt
+++ Fish/Player/strategy.rkt
@@ -9,22 +9,6 @@
 
 #;(provide ...)
 
-; Programming Task For this assignment, you are to implement a strategy component that takes care of
-; two decisions:
-
-; a choice of action for the player whose turn it is
-; It picks the action that realizes the minimal maximum gain after looking ahead N > 0 turns for this
-; player in the game tree for the current state.
-; The minimal maximum gain after N turns is the highest score a player can make after playing the
-; specified number of turns—assuming that all opponents pick one of the moves that minimizes the
-; player’s gain.
-
-; Tie Breaker If several different actions can realize the same gain, the strategy moves the penguin
-; that has the lowest row number for the place from which the penguin is moved and, within this row,
-; the lowest column number. In case this still leaves the algorithm with more than one choice, the
-; process is repeated for the target field to which the penguins will move. Why is this process
-; guaranteed to stop with a single action?
-
 ;; +-------------------------------------------------------------------------------------------------+
 ;; PROVIDED
 
@@ -42,41 +26,11 @@
                            (valid-tile? placement board)))
     placement))
 
-
-;; determine-move : game? natural? -> move?
+;; get-move : game? natural? -> move?
 ;; Applies the maximin algorithm to determine the best move for the current player, searching up to
 ;; depth turns for the current player
-(define (fish-maximin game depth)
+(define (get-move game depth)
   (first (maximin-search game depth (game-player-turn game))))
-
-;; maximin-search : game? posint? penguin-color? -> (list/c move (hash-of penguin-color? natural?))
-;; Determines the best move, and the scores resulting from that move, based on a maximin algorithm
-(define (maximin-search game remaining-depth original-player)
-  (define color (game-player-turn game))
-  (define new-depth (if (penguin-color=? (game-player-turn game) original-player)
-                        (sub1 remaining-depth)
-                        remaining-depth))
-  (define mapped-children
-    (apply-to-all-children game (λ (child) (maximin-h child new-depth original-player))))
-  
-  (for/foldr ([maybe-best #f])
-    ([(move score-map) (in-hash mapped-children)])
-    (cond[(not maybe-best) (list move score-map)]
-         [(> (hash-ref score-map color) (hash-ref (second maybe-best) color))
-          (list move score-map)]
-         [(< (hash-ref score-map color) (hash-ref (second maybe-best) color))
-          maybe-best]
-         [else (if (tiebreaker move (first maybe-best))
-                   (list move score-map)
-                   maybe-best)])))
-
-;; maximin-h : game-tree? natural? penguin-color? -> (hash-of penguin-color? natural?)
-;; Applies the heuristic if the game is over or the depth has been reached, otherwise applies maximin
-(define (maximin-h game remaining-depth original-player)
-  (if (or (end-game? game)
-          (zero? remaining-depth))
-      (fish-heuristic game)
-      (second (maximin-search game remaining-depth original-player))))
 
 ;; tiebreaker : move? move? -> boolean?
 ;; Given two distinct moves, determines which should be prioritized based on topmost row first, then
@@ -90,12 +44,42 @@
 ;; +-------------------------------------------------------------------------------------------------+
 ;; INTERNAL HELPERS
 
+;; maximin-search : game? posint? penguin-color? -> (list/c move (hash-of penguin-color? natural?))
+;; Determines the best move, and the scores resulting from that move, based on a maximin algorithm
+(define (maximin-search game remaining-depth original-player)
+  (define color (game-player-turn game))
+  (define new-depth (if (penguin-color=? (game-player-turn game) original-player)
+                        (sub1 remaining-depth)
+                        remaining-depth))
+  (define mapped-children
+    (apply-to-all-children game (λ (child) (maximin-recur child new-depth original-player))))
+  
+  (for/foldr ([maybe-best #f])
+    ([(move score-map) (in-hash mapped-children)])
+    
+    (cond [(not maybe-best) (list move score-map)]
+          [(> (hash-ref score-map color) (hash-ref (second maybe-best) color))
+           (list move score-map)]
+          [(< (hash-ref score-map color) (hash-ref (second maybe-best) color))
+           maybe-best]
+          [else (if (tiebreaker move (first maybe-best))
+                    (list move score-map)
+                    maybe-best)])))
+
+;; maximin-recur : game-tree? natural? penguin-color? -> (hash-of penguin-color? natural?)
+;; Applies the heuristic if the game is over or the depth has been reached, otherwise applies maximin
+(define (maximin-recur game remaining-depth original-player)
+  (if (or (end-game? game)
+          (zero? remaining-depth))
+      (fish-heuristic game)
+      (second (maximin-search game remaining-depth original-player))))
+
 ;; fish-heuristic : game-tree? -> (hash-of penguin-color? natural?)
 ;; A mapping of each player's score in the game
 (define (fish-heuristic game-tree)
   (define players (state-players (if (end-game? game-tree)
                                      (end-game-state game-tree)
-                                     (game-state game-tree))))
+                                     (finalize-state (game-state game-tree)))))
   (for/hash ([player players])
     (values (player-color player) (player-score player))))
 
@@ -140,5 +124,81 @@
                                     (make-player BLACK 0 (list (make-posn 1 0)))
                                     (make-player WHITE 0 (list (make-posn 3 0))))))
    (make-posn 3 1))
-                              
-  )
+  ;; +--- get-move ---+
+  (check-equal? (get-move test-game-1 1) (make-move (make-posn 1 2) (make-posn 1 1)))
+  (check-equal? (get-move test-game-1 2) (make-move (make-posn 1 2) (make-posn 0 1)))
+  (check-equal? (get-move test-game-2 1) (make-move (make-posn 0 0) (make-posn 1 2)))
+  (check-equal? (get-move test-game-2 2) (make-move (make-posn 0 0) (make-posn 1 3)))
+  (check-equal? (get-move test-game-2 3) (make-move (make-posn 0 0) (make-posn 1 2)))
+  (check-equal? (get-move test-game-2 4) (make-move (make-posn 0 0) (make-posn 1 3)))
+  (check-equal? (get-move test-game-2 10) (make-move (make-posn 0 0) (make-posn 1 3)))
+  ;; +--- tiebreaker ---+
+  (check-true (tiebreaker (make-move (make-posn 3 3) (make-posn 0 0))
+                          (make-move (make-posn 3 3) (make-posn 0 1))))
+  (check-true (tiebreaker (make-move (make-posn 3 3) (make-posn 0 0))
+                          (make-move (make-posn 3 3) (make-posn 1 0))))
+  (check-true (tiebreaker (make-move (make-posn 0 0) (make-posn 3 3))
+                          (make-move (make-posn 0 1) (make-posn 3 3))))
+  (check-true (tiebreaker (make-move (make-posn 0 0) (make-posn 3 3))
+                          (make-move (make-posn 1 0) (make-posn 3 3))))
+
+  ;; Internal Helper Functions
+  ;; +--- maximin-search ---+
+  ;; Game 1 partial searches
+  (check-equal? (maximin-search test-game-1 1 WHITE)
+                (list (make-move (make-posn 1 2) (make-posn 1 1)) (hash WHITE 6 BLACK 1)))
+  ;; Game 1 complete searches
+  (check-equal? (maximin-search test-game-1 2 WHITE)
+                (list (make-move (make-posn 1 2) (make-posn 0 1)) (hash WHITE 7 BLACK 1)))
+  (check-equal? (maximin-search test-game-1 3 WHITE)
+                (list (make-move (make-posn 1 2) (make-posn 0 1)) (hash WHITE 7 BLACK 1)))
+  ;; Game 2 partial searches
+  (check-equal? (maximin-search test-game-2 1 BLACK)
+                (list (make-move (make-posn 0 0) (make-posn 1 2)) (hash BLACK 5 RED 3 WHITE 2)))
+  (check-equal? (maximin-search test-game-2 2 BLACK)
+                (list (make-move (make-posn 0 0) (make-posn 1 3)) (hash BLACK 8 RED 4 WHITE 5)))
+  (check-equal? (maximin-search test-game-2 3 BLACK)
+                (list (make-move (make-posn 0 0) (make-posn 1 2)) (hash BLACK 10 RED 5 WHITE 9)))
+  (check-equal? (maximin-search test-game-2 4 BLACK)
+                (list (make-move (make-posn 0 0) (make-posn 1 3)) (hash BLACK 13 RED 8 WHITE 10)))
+  ;; Game 2 complete searches
+  (check-equal? (maximin-search test-game-2 5 BLACK)
+                (list (make-move (make-posn 0 0) (make-posn 1 3)) (hash BLACK 15 RED 8 WHITE 11)))
+  (check-equal? (maximin-search test-game-2 6 BLACK)
+                (list (make-move (make-posn 0 0) (make-posn 1 3)) (hash BLACK 15 RED 8 WHITE 11)))
+  ;; +--- maximin-recur ---+
+  ;; Game 1 partial searches
+  (check-equal? (maximin-recur test-game-1 0 WHITE)
+                (hash BLACK 1 WHITE 2))
+  (check-equal? (maximin-recur test-game-1 1 WHITE)
+                (hash BLACK 1 WHITE 6))
+  ;; Game 1 complete searches
+  (check-equal? (maximin-recur test-game-1 2 WHITE)
+                (hash BLACK 1 WHITE 7))
+  (check-equal? (maximin-recur test-game-1 3 WHITE)
+                (hash BLACK 1 WHITE 7))
+  ;; Game 2 partial searches
+  (check-equal? (maximin-recur test-game-2 0 BLACK)
+                (hash BLACK 1 RED 3 WHITE 2))
+  (check-equal? (maximin-recur test-game-2 1 BLACK)
+                (hash BLACK 5 RED 3 WHITE 2))
+  (check-equal? (maximin-recur test-game-2 2 BLACK)
+                (hash BLACK 8 RED 4 WHITE 5))
+  (check-equal? (maximin-recur test-game-2 3 BLACK)
+                (hash BLACK 10 RED 5 WHITE 9))
+  (check-equal? (maximin-recur test-game-2 4 BLACK)
+                (hash BLACK 13 RED 8 WHITE 10))
+  ;; Game 2 complete searches
+  (check-equal? (maximin-recur test-game-2 5 BLACK)
+                (hash BLACK 15 RED 8 WHITE 11))
+  (check-equal? (maximin-recur test-game-2 6 BLACK)
+                (hash BLACK 15 RED 8 WHITE 11))
+  ;; +--- fish-heuristic ---+
+  (check-equal? (fish-heuristic test-game-1)
+                (hash BLACK 1 WHITE 2))
+  (check-equal? (fish-heuristic test-game-2)
+                (hash BLACK 1 RED 3 WHITE 2))
+  (check-equal? (fish-heuristic
+                 (create-game (make-state '((2))
+                                          (list (make-player WHITE 10 (list (make-posn 0 0)))))))
+                (hash WHITE 12)))

5c88b168b7c12c742781798b26199248a8bf43b2
Nick Thompson <git@nickt.dev>
2020-11-02 19:30:25 -0500

    Some xstrategy tests

--- 6/Other/xstrategy.rkt
+++ 6/Other/xstrategy.rkt
@@ -17,7 +17,7 @@
 ;; PROVIDED
 
 ;; xstrategy : -> void?
-;; 
+;; Read a JSON Depth-State from STDIN, and finds the best move (if possible), written to STDOUT
 (define (xstrategy)
   (define depth-state (parse-json-depth-state (read-json)))
   (define maybe-move (xstrategy-helper (second depth-state) (first depth-state)))
@@ -38,15 +38,42 @@
 
 ;; +-------------------------------------------------------------------------------------------------+
 ;; TESTS
-#;
 (module+ test
   (require lang/posn
            rackunit
            "../../Fish/Other/util.rkt")
 
+  ;; +--- xstrategy-helper ---+
+  ;; no moves at all
+  (check-false (xstrategy-helper (make-state '((0 0 1) (1 0 0))
+                                             (list (make-player RED 2 (list (make-posn 0 2)))
+                                                   (make-player WHITE 2 (list (make-posn 1 0)))))
+                                 1))
+  ;; no moves for current player
+  (check-false (xstrategy-helper (make-state '((0 0 1) (1 2 0))
+                                             (list (make-player RED 2 (list (make-posn 0 2)))
+                                                   (make-player WHITE 2 (list (make-posn 1 0)))))
+                                 2))
+  ;; one move left
+  (check-equal? (xstrategy-helper (make-state '((0 0 1) (1 2 0))
+                                              (list (make-player WHITE 2 (list (make-posn 1 0)))
+                                                    (make-player RED 2 (list (make-posn 0 2)))))
+                                  2)
+                (make-move (make-posn 1 0) (make-posn 1 1)))
+
+  ;; complex game
+  (check-equal? (xstrategy-helper (make-state '((1 2 3 0 3 1)
+                                                (2 0 4 3 1 3)
+                                                (1 4 3 1 2 0))
+                                              (list (make-player BLACK 0 (list (make-posn 0 0)))
+                                                    (make-player WHITE 0 (list (make-posn 2 4)))
+                                                    (make-player RED 0 (list (make-posn 1 5))))) 2)
+                (make-move (make-posn 0 0) (make-posn 1 2)))
+
   ;; Integration tests
   (check-integration xstrategy "../Tests/1-in.json" "../Tests/1-out.json")
+  #|
   (check-integration xstrategy "../Tests/2-in.json" "../Tests/2-out.json")
   (check-integration xstrategy "../Tests/3-in.json" "../Tests/3-out.json")
   (check-integration xstrategy "../Tests/4-in.json" "../Tests/4-out.json")
-  (check-integration xstrategy "../Tests/5-in.json" "../Tests/5-out.json"))
+  (check-integration xstrategy "../Tests/5-in.json" "../Tests/5-out.json")|#)
